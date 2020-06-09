48d8a182f028b3252e41a4ae72ce33b2b056398e
Mahnoor Rizwan <mahnoorrizwan411@gmail.com>
2020-06-09 15:18:11 +0500

    new example overloading and inheritance

--- Untitled5.cpp
+++ Untitled5.cpp
@@ -1,5 +1,5 @@
-
 #include <iostream>
+
 using namespace std;
 
 class classa
@@ -9,7 +9,10 @@ virtual void display()
 {
 cout<<"class a"<<endl;
 }
+
+virtual void objects() =0;
 };
+
 
 class classb: public classa
 {
@@ -17,6 +20,10 @@ public:
 void display()
 {
 cout<<"class b \n";
+}
+
+void objects(){
+	cout<< "you choose a bat" << endl;
 }
 
 int sum(int a, int b)
@@ -37,19 +44,58 @@ return d;
 
 };
 
-int main() 
+
+class classc: public classa
 {
-cout<<"heyyyyy \n\n";
-classa a;
+public:
+	
+void objects(){
+	cout<< "you choose a cat" << endl;
+}
+
+};
+
+
+
+
+
+int main() {
+	
+	
+	// overloading objects function
+	
+cout<<"hey! lets play a game \n\n";
+cout<<"It is a game of luck! what do you want bat or cat??? \n\n";
+cout<<"1-b \n\n2-c \n\n";
+int choice;
+cout<< "enter 1 or 2 = ";
+cin>> choice;
+classa *a;
 classb b;
+classc c;
+
+if (choice==1){
+	a=&b;
+	a->objects();
+}
+else{
+	a=&c;
+	a->objects();
+}
+
+
+
+cout<<"\n\n\n\n\nsome examples of polymorphism\n\n";
+
+classa *aa;
 
 // function overriding 
-a.display();
+aa->display();
 b.display();
 
 //runtime polymorphism virtual func
 classa *p = new classb;  
-p->display(); // Envoked suring runtime 
+p->display();
 
 
 //function overloading
@@ -58,7 +104,7 @@ sum= b.sum(2,4,8);
 cout<<"3 parameter sum = "<<sum<<endl;
 sum=b.sum(2,4);
 cout<<"2 parameter sum = "<<sum<<endl;
-return 0;
+
 
 
 
